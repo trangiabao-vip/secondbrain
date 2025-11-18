e173fc390e3b103efd868c2a56a9f17847850455
gia tran <trangiabao4796@gmail.com>
2025-11-18 07:44:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6571219821-5a882",
+  "appId": "1:106549901014:web:7b40d78613d45c6e077b77",
+  "apiKey": "AIzaSyBVYw4pyp0ge3ZpdpeFBQL7n1YsiX7edHY",
+  "authDomain": "studio-6571219821-5a882.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "106549901014"
+};
