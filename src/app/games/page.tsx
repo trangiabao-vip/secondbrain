e6d37fd19abc4cd25d4a9607ac7bcdc667cc7186
gia tran <trangiabao4796@gmail.com>
2025-11-20 08:36:05 +0000

    Try fixing this error: `Runtime Error: Error: React is not defined. Erro

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -1,10 +1,10 @@
 'use client';
+import React from 'react';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Icons } from "@/components/icons";
 import Link from "next/link";
 import { useAppContext } from "@/contexts/AppContext";
-import AppPage from "../page";
 
 const games = [
   {
