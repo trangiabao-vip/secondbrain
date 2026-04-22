65094060c936f7c2c0dcad22aab470b74f07b360
gia tran <trangiabao4796@gmail.com>
2026-04-22 13:41:01 +0000

    sao t cài trước 5 phút bắt đầu mà có thấy thông báo gì đâu

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,29 +1,21 @@
-// This file needs to be in the public directory.
-importScripts("https://www.gstatic.com/firebasejs/11.9.1/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/11.9.1/firebase-messaging-compat.js");
+// Import the Firebase app and messaging packages
+import { initializeApp } from 'firebase/app';
+import { getMessaging } from 'firebase/messaging/sw';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    "projectId": "studio-6571219821-5a882",
-    "appId": "1:106549901014:web:7b40d78613d45c6e077b77",
-    "apiKey": "AIzaSyBVYw4pyp0ge3ZpdpeFBQL7n1YsiX7edHY",
-    "authDomain": "studio-6571219821-5a882.firebaseapp.com",
-    "messagingSenderId": "106549901014"
+  "projectId": "studio-6571219821-5a882",
+  "appId": "1:106549901014:web:7b40d78613d45c6e077b77",
+  "apiKey": "AIzaSyBVYw4pyp0ge3ZpdpeFBQL7n1YsiX7edHY",
+  "authDomain": "studio-6571219821-5a882.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "106549901014"
 };
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
 
-firebase.initializeApp(firebaseConfig);
-
-const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage(function(payload) {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/icon.svg'
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
+// The service worker doesn't need much more than this for basic background notifications.
+// Firebase Messaging will automatically handle displaying notifications sent from a server.
+// You could add a 'backgroundMessageHandler' here for custom logic, but it's not needed for now.
